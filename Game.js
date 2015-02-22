4658d4dee6e1afb377cff1db966eb9fe154eff00
Jim Veneskey <github@strifecrafter.com>
2015-02-22 11:03:16 -0500

    Tutorial 03-03 - Trying to move the bunnies

--- Game.js
+++ Game.js
@@ -27,8 +27,38 @@ BunnyDefender.Game.prototype = {
             b.animations.add('Rest', this.game.math.numberArray(1,58));
             b.animations.add('Walk', this.game.math.numberArray(68,107));
             b.animations.play('Rest', 24, true);
+            this.assignBunnyMovement(b);
         }
     },
+    
+    assignBunnyMovement: function(b) {
+        bposition = Math.floor(this.rnd.realInRange(50,  this.world.width-50));
+        bdelay = this.rnd.IntegerInRange(2000, 6000);
+        if (bposition < b.x) {
+            b.scale.x = 1;
+        } else {
+            b.scale.x = -1;
+        }
+        t = this.add.tween(b).to({x:position}, 3500, Phaser.Easing.Quadratic.InOut, true, bdelay);
+        t.onStart.add(this.startBunny, this);
+        t.onComplete.add(this.stopBunny, this);
+    },
+    
+    starBunny: function(b) {
+        b.animations.stop('Play');
+        b.animations.play('Walk', 24, true);
+    },
+    
+    stopBunny: function(b) {
+        b.animations.stop('Walk');
+        b.animations.play('Rest', 24, true);
+        this.assignBunnyMovement(b);
+    },
+    
+    
+    
+    
+    
     
     update: function() {}
     
