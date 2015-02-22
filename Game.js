9401cdca28ab9a2c75bf281ff484308b50eb1b07
Jim Veneskey <github@strifecrafter.com>
2015-02-21 20:25:30 -0500

    Tutorial 03-01 - Typo correction

--- Game.js
+++ Game.js
@@ -2,7 +2,17 @@ BunnyDefender.Game = function(game) {};
 
 BunnyDefender.Game.prototype = {
     
-    crate: function() {},
+    create: function() {
+
+        this.buildWorld()
+            
+    },
     
+    buildWorld: function() {
+        this.add.image(0,0, 'sky');
+        this.add.image(0,800, 'hill');
+    },
+        
     update: function() {}
+        
 };
--- Preloader.js
+++ Preloader.js
@@ -14,6 +14,9 @@ BunnyDefender.Preloader.prototype = {
         this.titleText.anchor.setTo(0.5, 0.5);
         this.load.image('titlescreen','images/TitleBG.png');
         this.load.bitmapFont('eightbitwonder','fonts/eightbitwonder.png', 'fonts/eightbitwonder.fnt');
+        this.load.image('hill', 'images/hill.png');
+        this.load.image('sky','images/sky.png');
+        
     },
     
     create: function () {
